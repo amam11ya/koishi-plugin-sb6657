cb55c109ebda4ff46f4807b514f76e7a168b54bc
amam11ya <128252162+amam11ya@users.noreply.github.com>
2024-08-14 14:15:06 +0800

    Version 1.0.0

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 [![npm](https://img.shields.io/npm/v/@amam1ya/koishi-plugin-sb6657?style=flat-square)](https://www.npmjs.com/package/@amam1ya/koishi-plugin-sb6657)
 
 玩机器弹幕烂梗生成器（数据来自 6657超级大爆 - sb6657.cn）
+
+## 使用方法
+
+- sb6657 随机生成一条玩机器弹幕烂梗
+
+## Acknowledgments
+
+- [6657超级大爆](https://sb6657.cn/)
+- [koishi](https://koishi.chat/)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { Context, Schema } from 'koishi'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { Context, h, Random, Schema } from 'koishi'
 
 export const name = 'sb6657'
 
@@ -6,6 +8,19 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+async function getData() {
+  const dataObject = JSON.parse(
+    readFileSync(join(__dirname, '/assets/6657data.json'), 'utf8'),
+  )
+  return Object.values(dataObject).flat()
+}
+
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.command('sb6657', '生成6657烂梗').action(async (props) => {
+    const data = await getData()
+    const { session } = props
+    const msg = [h('text', { content: `${Random.pick(data)}` })]
+    session.send(msg)
+  })
 }
